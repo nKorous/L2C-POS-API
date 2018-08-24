64c95e1e8314dbbd345712f78d1cb65e79f302f7
Nathaniel Korous <NeightKorous@Nathaniels-MBP.localdomain>
2018-08-23 22:10:28 -0600

    set up some items

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ let express = require('express')
 let cors = require('cors')
 let bodyParser = require('body-parser')
 
+
+let app = express()
+//App.use
+app.use(cors())
+app.use(bodyParser.json())
+
+//localVar
+let PORT = process.env.PORT || 9000
+
+//data
+let items = require('./data/items.json')
+
+//app listens on 9000
+app.listen(PORT, function(){
+    console.log(`listening on ${PORT}`)
+})
+
+app.get('/items', async (req, res) => {
+    res.status(200).send(items)
+})
+
+
+
+
